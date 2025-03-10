f2d02f8b3dd6d368be2eddeea4dcc57432c35eb7
JinjinBread <pge0720@naver.com>
2025-03-10 15:01:08 +0900

    init

---
+++ src/main/java/com/example/ticketing/health/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.example.ticketing.health.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String checkHealth() {
+        return "OK";
+    }
+}
